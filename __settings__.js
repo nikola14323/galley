958487aca61466bafed9ba92009b0928bee77723
nikola14323 <nak.milow@web.de>
2025-07-30 02:53:42 +0200

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2289198.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 243007261, 243007210, 243007226, 243007262, 243007260, 243007264, 243007233, 243007211, 243007217, 243007267, 243007229, 243007268, 243007273, 243007271, 243007199 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
